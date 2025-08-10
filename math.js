d6cb0a8c96c3131300e4dce04badebb8e3b05309
DiegoCasignia <alejodiego536@gmail.com>
2025-08-09 21:31:27 -0500

    error intencional en la funcion isPrime

--- math.js
+++ math.js
@@ -3,7 +3,7 @@ function isPrime(num) {
     for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) return false;
     }
-    return true;
+    return false;
 }
 
 function reverseString(str) {
